1c5d0091e3cadb4995a57feea96a558291d68961
lastmaj <ahmed.mejb@gmail.com>
2023-05-10 01:03:15 +0100

    remove font imports

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,14 +3,6 @@ import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-const interRegular = fetch(new URL('./Inter-Regular.ttf', import.meta.url)).then((res) =>
-  res.arrayBuffer()
-);
-
-const interBold = fetch(new URL('./Inter-Bold.ttf', import.meta.url)).then((res) =>
-  res.arrayBuffer()
-);
-
 export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
   try {
 
